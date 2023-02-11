64b43a4566b4a97a00f5b6e13608fcf540e8c3c7
Bayramali1999 <50273925+Bayramali1999@users.noreply.github.com>
2023-02-11 12:35:58 +0500

    Initial commit

---
+++ app/src/main/java/com/example/opencvtest/MainActivity.java
@@ -0,0 +1,166 @@
+package com.example.opencvtest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final int CAM_REQ_CODE = 123;
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+
+    Mat mRgba, intermediaMat, mGray, hierarchy;
+    List<MatOfPoint> contours;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAM_REQ_CODE && grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            loadCamera();
+        } else {
+            Toast.makeText(this, "You must to give permission for using this app", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void loadCamera() {
+
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+        cameraBridgeViewBase.setVisibility(View.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+                switch (status) {
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+
+            }
+        };
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(this, "There is some error", Toast.LENGTH_SHORT).show();
+        } else {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(width, height, CvType.CV_8UC4);
+        intermediaMat = new Mat(width, height, CvType.CV_8UC4);
+        mGray = new Mat(width, height, CvType.CV_8UC1);
+        hierarchy = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+        intermediaMat.release();
+        mGray.release();
+        hierarchy.release();
+    }
+
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = new Mat();
+        contours = new ArrayList<>();
+
+        Imgproc.Canny(mRgba, intermediaMat, 80, 100);
+        Imgproc.findContours(intermediaMat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
+
+        hierarchy.release();
+
+        for (int i = 0; i < contours.size(); i++) {
+            MatOfPoint2f approxCurve = new MatOfPoint2f();
+            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+
+            double approxDistance = Imgproc.arcLength(contour2f, true) * 0.01;
+            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+            MatOfPoint points = new MatOfPoint(approxCurve.toArray());
+
+            Rect rect = Imgproc.boundingRect(points);
+            double height = rect.height;
+            double width = rect.width;
+
+
+            if (height > 300 && width > 300) {
+                Imgproc.rectangle(mRgba, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0, 0), 5);
+                Imgproc.putText(mRgba, "There is a object ", rect.tl(), Core.FONT_HERSHEY_SCRIPT_SIMPLEX, 2,
+                        new Scalar(0, 255, 0, 0), 4);
+            }
+        }
+        return mRgba;
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CAM_REQ_CODE);
+        } else {
+            loadCamera();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+}

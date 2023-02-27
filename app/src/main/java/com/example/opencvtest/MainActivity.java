73c1c03d02769602031bf07b4bac76c37baeccd9
Bayramali1999 <50273925+Bayramali1999@users.noreply.github.com>
2023-02-28 01:29:02 +0500

    sdf
    asfd

--- app/src/main/java/com/example/opencvtest/MainActivity.java
+++ app/src/main/java/com/example/opencvtest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.opencvtest;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,7 @@ import androidx.core.content.ContextCompat;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -35,12 +37,19 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     BaseLoaderCallback baseLoaderCallback;
 
     Mat mRgba, intermediaMat, mGray, hierarchy;
+
+    int activeCamera = CameraBridgeViewBase.CAMERA_ID_BACK;
     List<MatOfPoint> contours;
 
+    View view;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.l_corner_view);
     }
 
 
@@ -58,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         cameraBridgeViewBase = findViewById(R.id.cameraView);
         cameraBridgeViewBase.setVisibility(View.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
-
+        cameraBridgeViewBase.setCameraIndex(this.activeCamera);
         baseLoaderCallback = new BaseLoaderCallback(this) {
             @Override
             public void onManagerConnected(int status) {
@@ -105,8 +114,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         mGray = new Mat();
         contours = new ArrayList<>();
 
-
-        Imgproc.Canny(mRgba, intermediaMat, 80, 100);
+        Imgproc.Canny(mRgba, intermediaMat, 100, 100);
         //Pointdagi 0 obyektni burchlari siljishi
         Imgproc.findContours(intermediaMat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
 
@@ -130,7 +138,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             //height and width rendering rectangles size
             int x = rect.x;
             int y = rect.y;
-            if ((x > 10 && y > 0) && (x < 150 && y < 200)) {
+//            view.setX(rect.x);
+//            view.setY(rect.y);
+
+            if (width>200 && height>200) {
 
                 Log.d("TAG_CAM_FRAME", "rect.height" + rect.height);
                 Log.d("TAG_CAM_FRAME", "rect.width" + rect.width);
@@ -141,8 +152,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
 
                 Imgproc.rectangle(mRgba, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0, 0), 5);
-                Imgproc.putText(mRgba, "a", rect.tl(), Core.FONT_HERSHEY_SCRIPT_SIMPLEX, 2,
-                        new Scalar(0, 255, 0, 0), 4);
+//                Imgproc.putText(mRgba, "a", rect.tl(), Core.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, new Scalar(0, 255, 0, 0), 4);
             }
         }
         return mRgba;
